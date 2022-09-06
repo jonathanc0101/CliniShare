8799c2fb29ae6114aeb157291e066d73356d1066
Jonathan Cavia <jonathancavia0101@gmail.com>
2022-09-06 20:09:28 -0300

    dos puertos en lugar de 1

--- broadcast.js
+++ broadcast.js
@@ -7,15 +7,11 @@ var listener = dgram.createSocket("udp4");
 let message = JSON.stringify(ipsGetter());
 
 
-listener.bind(9999,undefined,function(){
-    console.log("listening");
-});
-
 socket.bind(9999, undefined, function () {
     socket.setBroadcast(true);
 });
 
-socket.send(message, 0, message.length, 9999 , "255.255.255.255", function (err, bytes) {
+socket.send(message, 0, message.length, 10000 , "255.255.255.255", function (err, bytes) {
     socket.close();
 });
 
@@ -23,4 +19,8 @@ listener.on("message",function(message,rinfo){
     console.log('Message from: ' + rinfo.address + ':' + rinfo.port +' - ' + message);
 });
 
+listener.bind(10000,undefined,function(){
+    listener.setBroadcast(true);
+    console.log("listening");
+});
 
