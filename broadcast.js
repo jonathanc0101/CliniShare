6c9d7b102203a959c31d9444aa31b085057a2809
Jonathan Cavia <jonathancavia0101@gmail.com>
2022-09-06 19:02:31 -0300

    getip

--- broadcast.js
+++ broadcast.js
@@ -2,12 +2,16 @@ var dgram = require('dgram');
 
 var socket = dgram.createSocket("udp4");
 
-let message = "HOLA MUNDO ESTOY HACIENDO UN BROADCAST"
+let message = "HOLA MUNDO ESTOY HACIENDO UN BROADCAST";
 
 socket.bind(9999, undefined, function () {
     socket.setBroadcast(true);
+
+    console.log(JSON.stringify(socket.address()));
+    
 });
 
 socket.send(message, 0, message.length, 9999    , "255.255.255.255", function (err, bytes) {
+    console.log(JSON.stringify(socket.address()));
     socket.close();
 });
