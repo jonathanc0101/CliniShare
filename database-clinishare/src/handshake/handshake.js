e172c0c95dd0481025b1e1a7d770d697225fdfff
Jonathan Cavia <jonathancavia0101@gmail.com>
2022-09-10 16:46:30 -0300

    axios downgrade

--- database-clinishare/src/handshake/handshake.js
+++ database-clinishare/src/handshake/handshake.js
@@ -7,14 +7,14 @@ export function handleNewComputer(computadora) {
     for (let ip in computadora.IPS) {
         console.log("IP" + computadora.IPS[ip]);
 
-        const getMethodString = 'http://' + computadora.IPS[ip].toString().trim() + ':' + SERVER_BD_PORT + '/clinishare'
+        const getMethodString = 'http://' + computadora.IPS[ip].toString().trim() + ':' + SERVER_BD_PORT.toString().trim() + '/clinishare'
         console.log(getMethodString);
 
         axios
             .get(getMethodString)
             .then(res => {
-                // console.log(`statusCode: ${res.status}`);
                 console.log(res);
+                console.log("DATA " + JSON.stringify( res.data));
             })
             .catch(error => {
                 console.error(error);
