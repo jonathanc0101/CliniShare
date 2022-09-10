6ed4a8f68529e33e058af363bc09d20708933f1d
Jonathan Cavia <jonathancavia0101@gmail.com>
2022-09-10 16:03:40 -0300

    .then

--- database-clinishare/src/handshake/handshake.js
+++ database-clinishare/src/handshake/handshake.js
@@ -2,31 +2,28 @@ import axios from "axios";
 import { SERVER_BD_PORT, INITIAL_RESPONSE } from "../UDP/constants.js";
 import emitter from "../eventos/eventEmitter.js";
 
-export async function handleNewComputer(computadora){
+export function handleNewComputer(computadora){
 
-    try {     
         for(let ip in computadora.IPS){
             console.log("IP" + computadora.IPS[ip]);
             
-            let getMethodString = computadora.IPS[ip] + "%3" + SERVER_BD_PORT + "/clinishare";
+            let getMethodString = computadora.IPS[ip] + ":" + SERVER_BD_PORT + "/clinishare";
             console.log(getMethodString);
 
-            let res = await axios.get(getMethodString);
-            console.log((res));
-
-            let data = res.data;
-
-            console.log(data);
+            axios.get(getMethodString).then(res => {
+                // console.log(`statusCode: ${res.status}`);
+                console.log(res);
+              })
+              .catch(error => {
+                console.error(error);
+              });
         }
 
         // emitter.emit("new_valid_computer", )
 
-    } catch (error) {        
-        console.log(error);
-    }
 }
 
-export async function getInitialResponse(req, res) {
+export function getInitialResponse(req, res) {
     res.send(JSON.stringify({"INITIAL_RESPONSE":INITIAL_RESPONSE}));
 }
 
