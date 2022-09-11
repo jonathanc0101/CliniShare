12d237c721e730be1cca024e1e14bd9fc7515fd9
Jonathan Cavia <jonathancavia0101@gmail.com>
2022-09-11 15:20:15 -0300

    sincronizar

--- database-clinishare/src/sincronizacion/sincronizar.js
+++ database-clinishare/src/sincronizacion/sincronizar.js
@@ -3,15 +3,34 @@ import { SERVER_BD_PORT} from "../UDP/constants.js";
 import emitter from "../eventos/eventEmitter.js";
 
 export function sincronizar(computadora) {
-        const postMethodString = 'http://' + computadora.ip.toString().trim() + ':' + SERVER_BD_PORT.toString().trim() + '/sincronizar';
+        const postSincronicemosString = 'http://' + computadora.ip.toString().trim() + ':' + SERVER_BD_PORT.toString().trim() + '/sincronizar';
 
-        console.log(postMethodString);
+        const getDNISString = 'http://' + computadora.ip.toString().trim() + ':' + SERVER_BD_PORT.toString().trim() + '/pacientes/all/dnis';
+
+        axios
+            .get(getDNISString)
+            .then(res => {
+                if(res.data.count === 0){ //si no tiene pacientes no hay nada que hacer
+                    return //chau chau adios
+                }
+                
+                let dnisDePacientes = res.data;
+                console.log(dnisDePacientes);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
+        console.log("url de sincronizar: " + postSincronicemosString);
+
         // axios
-        //     .post(postMethodString)
+        //     .post(postMethodString,{
+                
+        //       })
         //     .then(res => {
-        //         if(res.data.INITIAL_RESPONSE === INITIAL_RESPONSE ){
+        //         if(??){
         //             // hacemosAlgo
-        //             emitter.emit("new_valid_computer", {nombre:computadora.nombre, ip:computadora.IPS[ip]})
+        //             emitter.emit("??", {???})
         //             return;
         //         }
         //     })
