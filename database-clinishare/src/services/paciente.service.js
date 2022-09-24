2d2a6b0a1e94d96130685a9586b9c975dd8997f7
nicole-alvarado <alvaradonicole123@hotmail.com>
2022-09-23 22:05:35 -0300

    service createEvento, ignorar los comentarios

--- database-clinishare/src/services/paciente.service.js
+++ database-clinishare/src/services/paciente.service.js
@@ -2,7 +2,6 @@ import { Paciente } from "../models/Paciente.js";
 import { HistoriaClinica } from "../models/HistoriaClinica.js";
 import { sequelize } from "../database/database.js";
 
-
 export const PacientesService = {
   getPacientes: () => getPacientesFromModel(),
   createPaciente: (paciente) => createPacienteFromModel(paciente),
@@ -25,65 +24,63 @@ async function getPacientesFromModel() {
 }
 
 async function createPacienteFromModel({ nombre, apellido, dni }) {
-
   try {
     //IMPORTANTE (para el futuro), COMPARAR Y DEJARLE AL USUARIO DECIDIR
     let newPacienteAux = {};
-    
+
     await sequelize.transaction(async (t) => {
-
-      const newPaciente = await Paciente.create({
-        nombre,
-        apellido,
-        dni,
-      },
+      const newPaciente = await Paciente.create(
         {
-          transaction: t
-        });
-
-      const nuevaHistoria = await HistoriaClinica.create({pacienteId:newPaciente.id},
+          nombre,
+          apellido,
+          dni,
+        },
         {
-          transaction: t
-        });
+          transaction: t,
+        }
+      );
 
-        newPacienteAux = {
+      //Se crea la historia clíínica del paciente
+      await HistoriaClinica.create(
+        { pacienteDni: newPaciente.dni },
+        {
+          transaction: t,
+        }
+      );
+
+      newPacienteAux = {
         nombre: newPaciente.nombre,
         apellido: newPaciente.apellido,
         dni: newPaciente.dni,
       };
-      
-    });  
+    });
 
     return newPacienteAux;
-    
-   } catch (error) {
-    console.log(error);
-    return {error}
-
+  } catch (error) {
+    console.log("No se pudo cargar el paciente. " + error);
+    return { error };
   }
 }
-
 
 async function getPacienteByDniFromModel(dniABuscar) {
   // let { dniABuscar } = req.body;
   const paciente = await Paciente.findAll({
     where: {
-      dni: dniABuscar
+      dni: dniABuscar,
     },
-    attributes: ['id', 'nombre', 'apellido', 'dni']
+    attributes: ["id", "nombre", "apellido", "dni"],
   });
 
   if (!paciente) {
-    return ("Paciente no encontrado");
-  }
-  else {
+    return null;
+  } else {
     return paciente;
   }
 }
 
 async function getDnisDePacientesFromModel() {
   const pacientes = await Paciente.findAll({
-    attributes: ['dni']
+    attributes: ["dni"],
   });
 
   if (pacientes.length === 0) {
@@ -100,9 +97,10 @@ async function getInterseccionDNISFromModel(dnis) {
   const newPacientes = pacientes.map((x) => x.dni);
   const newDnis = dnis.map((x) => x.dni);
 
-
-  dnisInterseccion = newDnis.filter(value => newPacientes.includes(value));
-  dnisInterseccion = dnisInterseccion.filter(value => newDnis.includes(value));
+  dnisInterseccion = newDnis.filter((value) => newPacientes.includes(value));
+  dnisInterseccion = dnisInterseccion.filter((value) =>
+    newDnis.includes(value)
+  );
 
   console.log("Dnis obtenidos en la INTERSECCIÓN: " + dnisInterseccion);
 
@@ -111,16 +109,15 @@ async function getInterseccionDNISFromModel(dnis) {
 
 async function getPacientesPorDnisFromModel(dnisPacientes) {
   if (dnisPacientes.length === 0) {
-    return []
+    return [];
   }
 
   let todosLosPacientes = await getPacientesFromModel();
-  
+
   // FILTRAR
-  let pacientesFiltrados = todosLosPacientes.filter(value => dnisPacientes.includes(value.dni));
+  let pacientesFiltrados = todosLosPacientes.filter((value) =>
+    dnisPacientes.includes(value.dni)
+  );
 
   return pacientesFiltrados;
-
 }
-
-
