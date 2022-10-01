964f373beeb9a51d404f5c02241d3b1494531c11
JC <jonathancavia0101@gmail.com>
2022-10-01 11:20:55 -0300

    rutas

--- frontend/src/API backend/rutas.js
+++ frontend/src/API backend/rutas.js
@@ -1,11 +1,12 @@
 const puertoServidorBackend = 3000;
 
 const rutas = {
-    nuevoPaciente: "pacientes/new"
+    nuevoPaciente: "pacientes/new",
+    getPaciente : "pacientes/id/"
 };
 
-// for(let ruta in rutas){
-//     rutas[ruta] = "http://localhost:"+ puertoServidorBackend +"/" + rutas[ruta];
-// }
+for(let ruta in rutas){
+    rutas[ruta] = "http://localhost:"+ puertoServidorBackend +"/" + rutas[ruta];
+}
 
 export default rutas;
