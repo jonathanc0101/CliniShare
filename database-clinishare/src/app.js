4f1f20231783ee3b1c18339c08420be9cbb923ed
JC <jonathancavia0101@gmail.com>
2022-10-01 10:54:17 -0300

    cors

--- database-clinishare/src/app.js
+++ database-clinishare/src/app.js
@@ -1,19 +1,14 @@
-import express from 'express';
+import express from "express";
 import medicosRoutes from "./routes/medicos.routes.js";
 import handshakeRoutes from "./routes/handshakes.routes.js";
 import pacientesRoutes from "./routes/pacientes.routes.js";
 import eventosRoutes from "./routes/eventos.routes.js";
 import sincronizacionesRoutes from "./routes/sincronizaciones.routes.js"
-import cors from 'cors';
+import cors from "cors";
+
 
 const app = express();
 app.use(cors());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "localhost:5000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
 //middlewares
 app.use(express.json());
@@ -21,7 +16,6 @@ app.use(medicosRoutes);
 app.use(handshakeRoutes);
 app.use(pacientesRoutes);
 app.use(eventosRoutes);
-app.use(sincronizacionesRoutes)
-
+app.use(sincronizacionesRoutes);
 
 export default app;
