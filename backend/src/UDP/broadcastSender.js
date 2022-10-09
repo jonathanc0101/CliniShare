e826acf81c5afd4a7cb03a5903ceac2895f8c60f
JC <jonathancavia0101@gmail.com>
2022-10-09 14:44:51 -0300

    documentacion

--- backend/src/UDP/broadcastSender.js
+++ backend/src/UDP/broadcastSender.js
@@ -1,44 +1,56 @@
 import os from "os";
-import dgram from 'dgram';
+import dgram from "dgram";
 import ipsGetter from "./getIp.js";
-import { SERVER_PORT, MAGIC_STRING } from "./constants.js"
+import { SERVER_PORT, MAGIC_STRING } from "./constants.js";
 
-export const computadora = { "nombre": os.userInfo().username, 
-"IPS": ipsGetter() ,
-MAGIC_STRING: MAGIC_STRING,
+export const computadora = {
+  nombre: os.userInfo().username,
+  IPS: ipsGetter(),
+  MAGIC_STRING: MAGIC_STRING,
+  medicoUUID: "63b814ea-8a7a-4442-b2f9-0be3a40e2fdf",
 };
 
-
 function broadcastMessage(myMessage) {
-    var sender = dgram.createSocket("udp4");
+  var sender = dgram.createSocket("udp4");
 
-    sender.bind(undefined, undefined, function () {
-        sender.setBroadcast(true);
-    });
+  sender.bind(undefined, undefined, function () {
+    sender.setBroadcast(true);
+  });
 
-    sender.send(myMessage, 0, myMessage.length, SERVER_PORT, "255.255.255.255", function (err, bytes) {
-
-        sender.close();
-    });
+  sender.send(
+    myMessage,
+    0,
+    myMessage.length,
+    SERVER_PORT,
+    "255.255.255.255",
+    function (err, bytes) {
+      sender.close();
+    }
+  );
 }
-
 
 export function broadcastComputerData() {
-    // broadcasts the computer data to the network
-    broadcastMessage(JSON.stringify(computadora));
+  // broadcasts the computer data to the network
+  broadcastMessage(JSON.stringify(computadora));
 }
 
-export function responderBroadcast(computer){
-    // enviamos nuestros datos (nombre, ips, etc.) a la computadora que nos envió un broadcast
+export function responderBroadcast(computer) {
+  // enviamos nuestros datos (nombre, ips, etc.) a la computadora que nos envió un broadcast
 
-    var sender = dgram.createSocket("udp4");
+  var sender = dgram.createSocket("udp4");
 
-    
-    let mensajeComputadora = JSON.stringify(computadora);
+  let mensajeComputadora = JSON.stringify(computadora);
 
-    sender.bind(undefined, undefined);
+  sender.bind(undefined, undefined);
 
-    sender.send(mensajeComputadora, 0, mensajeComputadora.length, SERVER_PORT, computer.ip[0], function (err, bytes) {
-        sender.close();
-    });
+  sender.send(
+    mensajeComputadora,
+    0,
+    mensajeComputadora.length,
+    SERVER_PORT,
+    computer.ip[0],
+    function (err, bytes) {
+      sender.close();
+    }
+  );
 }
