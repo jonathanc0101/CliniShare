d233b7c294b4f5d5fe2dc935143811c5f2d116b5
nicole-alvarado <alvaradonicole123@hotmail.com>
2022-10-16 14:54:16 -0300

    login...

--- frontend/src/componentes/LoginForm.jsx
+++ frontend/src/componentes/LoginForm.jsx
@@ -19,41 +19,43 @@ function LoginForm() {
   const [password, setPassword] = useState("");
   const [usuario, setUsuario] = useState({
     nombre: "",
-    token:"",
-    email:""
+    token: "",
+    email: "",
   });
-  const [error, setError] = useState("");
   const [datosValidos, setDatosValidos] = useState(false);
 
-//   useEffect(()=> {
-
-//   },[])
+    useEffect(()=> {
+        const loggedCliniShareUserJSON = window.localStorage.getItem("loggedCliniShareAppUser")
+        if(loggedCliniShareUserJSON){
+            const user = JSON.parse(loggedCliniShareUserJSON)
+            setUsuario(user);
+        }
+    },[])
 
   const handleLogin = async (event) => {
     event.preventDefault();
 
     try {
       const usuario = await api.login(correoElectronico, password);
+      console.log("Object: ",usuario);
       console.log(Object.keys(usuario).length !== 0);
-      if(Object.keys(usuario).length !== 0){
-        console.log("antes dentro del if ",datosValidos);
+      if (Object.keys(usuario).length !== 0) {
         setDatosValidos(true);
-        console.log("datos valid ",datosValidos);
+        const usuarioAux = {
+          nombre: usuario.medico.nombre,
+          token: usuario.token,
+          email: usuario.medico.email,
+        };
 
+        console.log("Usuario aux: ",usuarioAux);
+        window.localStorage.setItem(
+          "loggedCliniShareAppUser",
+          JSON.stringify(usuarioAux)
+        );
+        setUsuario(usuarioAux);
+        setCorreoElectronico("");
+        setPassword("");
       }
-
-    //   const usuarioAux = {
-    //     nombre: usuario.medico.nombre,
-    //     token: usuario.token,
-    //     email: usuario.email
-    //   }
-    //   window.localStorage.setItem(
-    //     "loggedCliniShareAppUser", JSON.stringify(usuarioAux)           
-    //   )
-      setUsuario(usuario);
-      setCorreoElectronico("");
-      setPassword("");
-
     } catch (error) {
       console.log(error);
     }
@@ -69,61 +71,62 @@ function LoginForm() {
   const btnstyle = { margin: "8px 0" };
   return (
     <>
-    {datosValidos?<Navigate to="/" /> :
-      <Grid>
-        <Paper elevation={10} style={paperStyle}>
-          <Grid align="center">
-            <Avatar style={avatarStyle}>
-              <AccountCircleIcon />
-            </Avatar>
-            <h2>Login</h2>
-          </Grid>
-          <TextField
-            type="text"
-            label="Correo electrónico"
-            placeholder="Ingrese su correo electrónico"
-            value={correoElectronico}
-            onChange={({ target }) => setCorreoElectronico(target.value)}
-            fullWidth
-            required
-          />
-          <br></br>
-          <br></br>
+      {datosValidos ? (
+        <Navigate to="/" />
+      ) : (
+        <Grid>
+          <Paper elevation={10} style={paperStyle}>
+            <Grid align="center">
+              <Avatar style={avatarStyle}>
+                <AccountCircleIcon />
+              </Avatar>
+              <h2>Login</h2>
+            </Grid>
+            <TextField
+              type="text"
+              label="Correo electrónico"
+              placeholder="Ingrese su correo electrónico"
+              value={correoElectronico}
+              onChange={({ target }) => setCorreoElectronico(target.value)}
+              fullWidth
+              required
+            />
+            <br></br>
+            <br></br>
 
-          <TextField
-            label="Password"
-            placeholder="Ingrese su contraseña"
-            type="password"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-            fullWidth
-            required
-          />
-          {/* <FormControlLabel
+            <TextField
+              label="Password"
+              placeholder="Ingrese su contraseña"
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              fullWidth
+              required
+            />
+            {/* <FormControlLabel
             control={<Checkbox name="checkedB" color="primary" />}
             label="Remember me"
           /> */}
-          <Button
-            onClick={handleLogin}
-            color="primary"
-            variant="contained"
-            style={btnstyle}
-            fullWidth
-            
-          >
-            Login
-          </Button>
+            <Button
+              onClick={handleLogin}
+              color="primary"
+              variant="contained"
+              style={btnstyle}
+              fullWidth
+            >
+              Login
+            </Button>
 
-          {/* <Typography>
+            {/* <Typography>
             <Link href="#">Forgot password ?</Link>
           </Typography>
           <Typography>
             {" "}
             Do you have an account ?<Link href="#">Sign Up</Link>
           </Typography> */}
-        </Paper>
-      </Grid>
-      }
+          </Paper>
+        </Grid>
+      )}
     </>
   );
 }
