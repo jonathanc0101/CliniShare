5ed922f9b6e4d923362e3485a9d90da2cfa0bd74
nicole-alvarado <alvaradonicole123@hotmail.com>
2022-10-22 10:28:17 -0300

    version anterior register

--- frontend/src/componentes/RegistroMedico.jsx
+++ frontend/src/componentes/RegistroMedico.jsx
@@ -3,10 +3,6 @@ import {
   Card,
   CardContent,
   Grid,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
   TextField,
   Typography,
 } from "@mui/material";
@@ -16,26 +12,11 @@ import { useState } from "react";
 import { api } from "../API backend/api";
 import { alertas } from "./alertas";
 import SaveIcon from "@mui/icons-material/Save";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
 function RegistroMedico() {
-  const [values, setValues] = useState({
-    mostrarPassword: "",
-  });
-
-  const handleClickShowPassword = () => {
-    setValues({
-      ...values,
-      mostrarPassword: !values.mostrarPassword,
-    });
-  };
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
 
   const isEmail = (email) =>
-    /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
 
   const [medico, setMedico] = useState({
     nombre: "",
@@ -48,10 +29,12 @@ function RegistroMedico() {
   });
 
   const guardar = async function () {
+
     if (!isEmail(medico.email)) {
       console.log("Email invalido");
       alertas.alertaEmailInvalido();
       return;
+      
     }
 
     const medicoGuardado = await api.guardarMedicoUsuario(medico);
@@ -99,65 +82,66 @@ function RegistroMedico() {
 
             {/* Nombre y apellido */}
             <Grid item xs={4} sm={4}>
-            <OutlinedInput
-              id="outlined-adornment-nombre"
-              type="text"
-              value={medico.nombre}
-              onChange={handleChange}
-              placeholder="Nombre"
-              fullWidth
-              margin="dense"
-              required
-            />
+              <TextField
+                label="Nombre"
+                type="text"
+                name="nombre"
+                value={medico.nombre}
+                onChange={handleChange}
+                margin="dense"
+                fullWidth
+                variant="outlined"
+                helperText="Campo obligatorio"
+              ></TextField>
             </Grid>
             <Grid item xs={4} sm={4}>
-            <OutlinedInput
-              id="outlined-adornment-apellido"
-              type="text"
-              value={medico.apellido}
-              onChange={handleChange}
-              placeholder="Apellido"
-              fullWidth
-              margin="dense"
-
-              required
-            />
+              <TextField
+                label="Apellido"
+                type="text"
+                name="apellido"
+                value={medico.apellido}
+                onChange={handleChange}
+                margin="dense"
+                fullWidth
+                variant="outlined"
+                helperText="Campo obligatorio"
+              ></TextField>
+            </Grid>
+          </Grid>
+          <Grid container direction="row" spacing={2}>
+            <Grid item xs={4} sm={4}>
+              <TextField
+                label="DNI"
+                type="text"
+                name="dni"
+                value={medico.dni}
+                onChange={handleChangeDni}
+                margin="dense"
+                fullWidth
+                variant="outlined"
+                helperText="Campo obligatorio"
+              ></TextField>
+            </Grid>
+            <Grid item xs={4} sm={4}>
+              <TextField
+                label="Mátricula habilitante"
+                type="text"
+                name="matricula"
+                value={medico.matricula}
+                onChange={handleChange}
+                margin="dense"
+                fullWidth
+                variant="outlined"
+                helperText="Campo obligatorio"
+              ></TextField>
             </Grid>
           </Grid>
           <br></br>
           <Grid container direction="row" spacing={2}>
             <Grid item xs={4} sm={4}>
-            <OutlinedInput
-              id="outlined-adornment-dni"
-              type="text"
-              value={medico.dni}
-              onChange={handleChangeDni}
-              placeholder="DNI"
-              fullWidth
-              margin="dense"
-
-              required
-            />
-
-            </Grid>
-            <Grid item xs={4} sm={4}>
-            <OutlinedInput
-              id="outlined-adornment-matricula"
-              type="text"
-              value={medico.matricula}
-              onChange={handleChange}
-              placeholder="Nombre"
-              fullWidth
-              required
-            />
-            </Grid>
-          </Grid>
-          <br></br>
-          <Grid container direction="row" spacing={2}>
-            <Grid item xs={4} sm={4}>
-              <InputLabel>Fecha de nacimiento</InputLabel>
               <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DesktopDatePicker
+                  label="Fecha de nacimiento"
                   name="fechaNacimiento"
                   value={medico.fechaNacimiento}
                   onChange={handleChangeFecha}
@@ -170,43 +154,33 @@ function RegistroMedico() {
 
           <Grid container direction="row" spacing={2}>
             <Grid item xs={2} sm={6}>
-            <OutlinedInput
-              id="outlined-adornment-email"
-              type="text"
-              value={medico.email}
-              onChange={handleChange}
-              placeholder="Correo electrónico"
-              fullWidth
-              required
-            />
+              <TextField
+                label="Correo electrónico"
+                type="text"
+                name="email"
+                value={medico.email}
+                
+                onChange={handleChange}
+                margin="dense"
+                fullWidth
+                variant="outlined"
+                helperText="Campo obligatorio"
+              ></TextField>
             </Grid>
           </Grid>
           <Grid container direction="row" spacing={2}>
-            <Grid item xs={4} sm={8}>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={values.mostrarPassword ? "text" : "password"}
-              value={medico.password}
-              onChange={handleChange}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {values.mostrarPassword ? (
-                      <VisibilityOff />
-                    ) : (
-                      <Visibility />
-                    )}
-                  </IconButton>
-                </InputAdornment>
-              }
-              placeholder="Contraseña"
-              required
-            />
+            <Grid item xs={2} sm={6}>
+              <TextField
+                label="Contraseña"
+                type="text"
+                name="password"
+                value={medico.password}
+                onChange={handleChange}
+                margin="dense"
+                fullWidth
+                variant="outlined"
+                helperText="Campo obligatorio"
+              ></TextField>
             </Grid>
           </Grid>
           <Grid container direction="row" spacing={2}>
