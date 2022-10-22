9271b0a606cafea8591d251602300fe2c24cb4cd
JC <jonathancavia0101@gmail.com>
2022-10-22 16:51:44 -0300

    obtener eventos por fecha de modificacion

--- backend/src/sincronizacion/sincronizar.js
+++ backend/src/sincronizacion/sincronizar.js
@@ -1,52 +1,66 @@
 import axios from "axios";
-import { SERVER_BD_PORT} from "../UDP/constants.js";
+import { SERVER_BD_PORT } from "../UDP/constants.js";
 import { PacientesService } from "../services/paciente.service.js";
 import emitter from "../eventos/eventEmitter.js";
+import { getComputadora } from "../UDP/constants.js";
 
 export async function sincronizar(computadora) {
-    
-        const postSincronicemosString = 'http://' + computadora.ip.toString().trim() + ':' + SERVER_BD_PORT.toString().trim() + '/sincronizar';
+  const computadoraLocal = await getComputadora();
+  const postSincronicemosString =
+    "http://" +
+    computadora.ip.toString().trim() +
+    ":" +
+    SERVER_BD_PORT.toString().trim() +
+    "/sincronizar";
 
-        const getDNISyNacimientosString = 'http://' + computadora.ip.toString().trim() + ':' + SERVER_BD_PORT.toString().trim() + '/pacientes/all/dnis;nacimientos';
+  const getDNISyNacimientosString =
+    "http://" +
+    computadora.ip.toString().trim() +
+    ":" +
+    SERVER_BD_PORT.toString().trim() +
+    "/pacientes/all/dnis;nacimientos";
 
+  //obtener DNIS para sincronizar con los que tenemos en comun
+  let dnisyNacimientosDePacientes = [];
+  await axios
+    .get(getDNISyNacimientosString)
+    .then((res) => {
+      if (res.data.count === 0) {
+        //si no tiene pacientes no hay nada que hacer
+        return; //chau chau adios
+      }
 
-        //obtener DNIS para sincronizar con los que tenemos en comun
-        let dnisyNacimientosDePacientes = [];
-        await axios
-            .get(getDNISyNacimientosString)
-            .then(res => {
-                if(res.data.count === 0){ //si no tiene pacientes no hay nada que hacer
-                    return //chau chau adios
-                }
-                
-                dnisyNacimientosDePacientes = res.data;
-            })
-            .catch(error => {
-                console.error(error);
-            });
+      dnisyNacimientosDePacientes = res.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
-            let dnisyFechasASincronizar = await PacientesService.getInterseccionDNISyFechas(dnisyNacimientosDePacientes);
-            
+  let dnisyFechasASincronizar =
+    await PacientesService.getInterseccionDNISyFechas(
+      dnisyNacimientosDePacientes
+    );
 
-        //obtener los datos a sincronizar
-        await axios
-            .post(postSincronicemosString,dnisyFechasASincronizar)
-            .then(res => {
-                if(!res.data){
-                    return //chau chau adios
-                }
-                if(res.data.length === 0){
-                    return //chau chau adios
-                }
+  //obtener los datos a sincronizar
+  await axios
+    .post(postSincronicemosString, {
+      medicoId: computadoraLocal.medicoId,
+      dnisyFechasASincronizar,
+    })
+    .then((res) => {
+      if (!res.data) {
+        return; //chau chau adios
+      }
+      if (res.data.length === 0) {
+        return; //chau chau adios
+      }
 
-                let datosPacientes = res.data;
-        
-                // hacemosAlgo
-                emitter.emit("datos_recibidos", datosPacientes)
-            })
-            .catch(error => {
-                console.error(error);
-            });
+      let datosPacientes = res.data;
 
+      // hacemosAlgo
+      emitter.emit("datos_recibidos", datosPacientes);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
-
