0fab13f2b4d8367b686ed771466e993a6b75bae9
JC <jonathancavia0101@gmail.com>
2022-11-12 16:54:58 -0300

    no recibir mis eventos

--- backend/src/sincronizacion/datosPacientes.js
+++ backend/src/sincronizacion/datosPacientes.js
@@ -5,36 +5,53 @@ import { Medico } from "../models/Medico.js";
 import { Evento } from "../models/Evento.js";
 
 export async function handleSincronizarPostRequest(req, res, next) {
-  res.send(await getDatosParaSincronizar(req.body.fecha,req.body.dnisyFechasASincronizar));
+  res.send(
+    await getDatosParaSincronizar(
+      req.body.fecha,
+      req.body.dnisyFechasASincronizar,
+      req.body.computadora
+    )
+  );
 }
 
-export async function getDatosParaSincronizar(fecha,dnisYFechas) {
-
-  if(!fecha){
+export async function getDatosParaSincronizar(fecha, dnisYFechas, computadora) {
+  let eventos = [];
+  if (!fecha) {
     //primera sincronizacion
-    return await EventosService.getEventosCompletosPorDnisYFechas(dnisYFechas);
-  }else{
-    return await EventosService.getEventosCompletosPorDnisYFechasAPartirDeFecha(dnisYFechas,fecha);
+    eventos = await EventosService.getEventosCompletosPorDnisYFechas(
+      dnisYFechas
+    );
+  } else {
+    eventos = EventosService.getEventosCompletosPorDnisYFechasAPartirDeFecha(
+      dnisYFechas,
+      fecha
+    );
   }
 
+  if ( Object.keys(eventos).length === 0) {
+    return {};
+  }
+  //filtramos por los medicos que tiene la maquina, los datos fluyen en una sola direccion
+  return EventosService.excluirPorIdsMedicos(eventos, computadora.medicosIds);
 }
 
 export async function actualizarDatos(datos) {
-  console.log("\n\nevento:datos a actualizar\n\n",datos);
+  console.log("\n\nevento:datos a actualizar\n\n", datos);
   //hay que resolver el tema de sincronizar los pacientes, que quede almacenada la version mas actualizada del paciente
   try {
     await sequelize.transaction(async (t) => {
       for (const evento of datos) {
-        await Medico.upsert(evento.medico,{transaction: t});
-        await PacientesService.upsertarPorDNIyNacimiento(evento.paciente,t);
+        await Medico.upsert(evento.medico, { transaction: t });
+        await PacientesService.upsertarPorDNIyNacimiento(evento.paciente, t);
         const eventoAux = {
           ...evento,
-          pacienteId: await PacientesService.getIdPorDniYNacimiento(evento.paciente),
+          pacienteId: await PacientesService.getIdPorDniYNacimiento(
+            evento.paciente
+          ),
           medicoId: evento.medicoId,
         };
-        await Evento.upsert(eventoAux,{transaction: t});
+        await Evento.upsert(eventoAux, { transaction: t });
       }
-
     });
 
     return true;
