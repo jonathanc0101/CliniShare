1045f0099199e9fee6c6e824faa7910433d24ddb
nicole-alvarado <alvaradonicole123@hotmail.com>
2022-12-04 16:43:00 -0300

    descargar pdf

--- frontend/src/componentes/DescargarPDF.js
+++ frontend/src/componentes/DescargarPDF.js
@@ -4,12 +4,35 @@ import { useParams } from "react-router-dom";
 import BotonVolver from "./Botones/BotonVolver";
 import PDFFile from "./PDFFIle";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import { useState } from "react";
 
 const DescargarPDF = ({ paciente }) => {
-  return ( 
-    <div>
- 
-      <PDFDownloadLink document={<PDFFile paciente={paciente} />} fileName="FORM">
+  const nombrePDF =
+    "Historia clínica-" + paciente.nombre + " " + paciente.apellido;
+
+  const [verPDF, setVerPDF] = useState(false);
+
+  const MenuPrueba = () => {
+    
+
+  };
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <Button
+        variant="dark"
+        onClick={() => {
+          setVerPDF(true);
+        }}
+      >Ver</Button>      
+      {verPDF ? (
+        <PDFViewer style={{ width: "100%", height: "90vh" }}>
+          <PDFFile paciente={paciente} />
+        </PDFViewer>
+      ) : null}
+      {/* <PDFDownloadLink
+        document={<PDFFile paciente={paciente} />}
+        fileName={nombrePDF}
+      >
         <IconButton
           color="primary"
           aria-label="upload picture"
@@ -17,7 +40,7 @@ const DescargarPDF = ({ paciente }) => {
         >
           <FileDownloadIcon />
         </IconButton>
-      </PDFDownloadLink>
+      </PDFDownloadLink> */}
     </div>
   );
 };
